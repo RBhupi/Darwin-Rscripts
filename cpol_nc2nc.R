c40725b90251f93e8e0c1dffedda8fb28b3ed27a
Bhupendra Raut <bhupendra.raut@gmail.com>
2016-03-02 14:56:43 +1100

    convert all files to single nc

--- cpol_nc2nc.R
+++ cpol_nc2nc.R
@@ -1,18 +1,35 @@
 
 library(ncdf4)
 library(plyr)
+library(stringr)
+#----------------------------------------------------------- FUNCTION DEFINITION
+#reads time from a list  of netcdf files and convert to seconds
+ncread_time_bulk <- function(filelist){
+    time_all <- laply(filelist, ncread_time)
+    invisible(time_all)
+}
+
+#reads time from a single netcdf file
+ncread_time <- function(fname){
+    ncfile <- nc_open(fname)
+    time <- ncvar_get(ncfile, varid = "time")
+    return(time)
+}
+#------------------------------------------------------------------------------#
 
 #get input file names
 indir <- "/home/bhupendra/data/darwin_radar/test/CPOL/"
 fpat <- "*.nc"
 flist <- Sys.glob(paste(indir, fpat, sep=""))
+#flist <- flist[1:2]
 print(paste("found", length(flist), "file(s)."))
+
 
 #make output directory
 outdir <- paste(indir, "outNC/", sep="")
 dir.create(outdir)
 
-#open netcdf file for reading
+#open netcdf file for reading dims
 infile <- nc_open(filename = flist[1])
 
 #read x y z dims
@@ -31,9 +48,8 @@ z_max1 <- ncvar_get(nc= infile, varid = "z_max")
 z_min1 <- ncvar_get(nc= infile, varid = "z_min")
 nz1 <- ncvar_get(nc= infile, varid = "nz")
 
-#read time and transform to POSIXct time unit "seconds since 1970-01-01"
-time_seconds<- 86400 * ncvar_get(nc = infile, varid = "time")
-time1 <- as.POSIXct(time_seconds, origin = "1970-01-01", tz="UTC")
+#read time from all teh files
+time_seconds<- 86400 * ncread_time_bulk(flist)
 
 data_date1 <- ncvar_get(nc = infile, varid = "data_date")
 data_time1 <- ncvar_get(nc = infile, varid = "data_time")
@@ -73,6 +89,9 @@ for(var in ivar_names){
     att<- unlist(ncatt_get(infile, varid=var, attname = "long_name"))
     ivar_longNames <- c(ivar_longNames, as.vector(att[2]))
 }
+#close input file hear
+nc_close(infile)
+
 
 # combin and rename
 ivar_df<-cbind(ivar_names, ivar_longNames)
@@ -90,25 +109,34 @@ out_ivar_list <- mlply(.data = ivar_df, .fun = ncvar_def, units = "", dim = list
 
 #------------------------------- WRITING TO THE FILE --------------------------#
 #open output netcdf file
-outfName <- paste(outdir, basename(flist[1]), sep="")
-ofile <- nc_create(filename = outfName, vars =append(out_fvar_list, out_ivar_list))
+outfName <- str_replace(flist[1], pattern = "_0000.nc", ".nc")
+outfPath <- paste(outdir, basename(outfName), sep="")
+ofile <- nc_create(filename = outfPath, vars =append(out_fvar_list, out_ivar_list))
 
-#get the data from input netcdf and put all float variables in output file
-for(var in fvar_names){
-  data <- ncvar_get(infile, varid = var)
-  #data <- replace(data, data==NA, -999.0)
-  ncvar_put(nc = ofile, varid = var, vals = data, start = c(1, 1, 1, 1),
-            count = c(length(x1), length(y1), length(z1), 1))
+print(paste("writing data in", basename(outfPath)))
+pb = txtProgressBar(min =1, max = length(flist), initial = 1, style = 3) #progress bar
+for (f in 1:length(flist)){
+    setTxtProgressBar(pb, f)
+    infile <- nc_open(flist[f])
+    #get the data from input netcdf and put all float variables in output file
+    for(var in fvar_names){
+        data <- ncvar_get(infile, varid = var)
+        #data <- replace(data, data==NA, -999.0)
+        ncvar_put(nc = ofile, varid = var, vals = data, start = c(1, 1, 1, f),
+                  count = c(length(x1), length(y1), length(z1), 1))
+    }
+
+    #get the data from input netcdf and put all integer variables in output file.
+    for(var in ivar_names){
+        data <- ncvar_get(infile, varid = var)
+        data <- replace(data, data==NA, -99)
+        ncvar_put(nc = ofile, varid = var, vals = data, start = c(1, 1, 1, f),
+                  count = c(length(x1), length(y1), length(z1), 1))
+    }
+    nc_close(infile)
 }
 
-#get the data from input netcdf and put all integer variables in output file
-for(var in ivar_names){
-    data <- ncvar_get(infile, varid = var)
-    data <- replace(data, data==NA, -99)
-    ncvar_put(nc = ofile, varid = var, vals = data, start = c(1, 1, 1, 1),
-              count = c(length(x1), length(y1), length(z1), 1))
-}
-
+print("")
 #close files
 nc_close(ofile)
-nc_close(infile)
+
