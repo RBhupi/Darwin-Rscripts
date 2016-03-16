b6c3f791671f4c0884f3c08c9982ef92662f6074
Bhupendra Raut <bhupendra.raut@gmail.com>
2016-03-16 14:19:48 +1100

    changed, tested and working version

--- cpol_nc2nc.R
+++ cpol_nc2nc.R
@@ -41,7 +41,7 @@ get_var_longnames <- function (infile, var_names) {
     att<- unlist(ncatt_get(infile, varid=var, attname = "long_name"))
     var_longNames <- c(var_longNames, as.vector(att[2]))
   }
-  
+
   # then combin and rename them to use in mlply
   varnames_df<-cbind(var_names, var_longNames)
   colnames(varnames_df) <- c("name", "longname")
@@ -59,26 +59,25 @@ flist_all <- Sys.glob(paste(indir, fpat, sep=""))
 print(paste(length(flist_all), "file(s) in the folder."))
 
 while(length(flist_all)>0) {
-  
+
   flist <- get_1dayFiles(flist_all)
-  #flist_all <- flist_all[!selectFiles] #remaining files are stored for next iteration
-  flist_all <- flist_all[!flist]  #this line is not tested. must be tested before use (I dont have data). I did it after making function for selecting files.
-  
+  flist_all <- flist_all[flist_all!=flist] #remaining files are stored for next iteration
+
   #open a netcdf file for reading dims
   infile <- nc_open(filename = flist[1])
-  
+
   #read x y z dims
   x1<- ncvar_get(nc = infile, varid = "x")
   y1<- ncvar_get(nc = infile, varid = "y")
   z1<- ncvar_get(nc = infile, varid = "z")
-  
+
   #read time from all the files
   time_seconds<- 86400 * laply(flist, ncread_time)
-  
+
   #also read radar latlon
   radar_lat1 <- ncvar_get(nc = infile, varid = "radar_latitude")
   radar_lon1 <- ncvar_get(nc = infile, varid = "radar_longitude")
-  
+
   #------------------------- MAKING DIMS & DATA VARIABLES -----------------------#
   #make output dim variables for x, y, z
   x_dim <- ncdim_def(name = "x", units = "km", vals = x1, longname = "distance from radar in zonal direction")
@@ -86,56 +85,57 @@ while(length(flist_all)>0) {
   z_dim <- ncdim_def(name = "z", units = "km", vals = z1, longname = "altitude")
   t_dim <- ncdim_def(name = "time", units = "seconds since 1970-01-01 00:00:00 UTC", calendar = "gregorian",
                      vals= time_seconds, longname = "Time of the scan", unlim = TRUE)
-  
+
   #also make radar lat-lon variables
   radar_lat <- ncvar_def(name = "radar_latitude", units = "degrees_north", dim = list(), prec = "float")
   radar_lon <- ncvar_def(name = "radar_longitude", units = "degrees_east", dim = list(), prec = "float")
   radar_latlon_var <- list(radar_lat, radar_lon)
-  
+
   #This is the list of important "float" variables in the file to read and write
   fvar_names <- c("zh", "vr", "ve", "sw", "zd", "ps", "rs", "kd", "rr", "do", "nw")
   fvar_df <- get_var_longnames(infile, fvar_names)
-  
+
   #also get long names for "integer" variables
   ivar_names <- c("hc", "cs","cm")
   ivar_df <- get_var_longnames(infile, ivar_names)
-  
+
   #close input file hear
   nc_close(infile)
-  
+
   # Now create all float netcdf variables at once using mlply
   out_fvar_list <- mlply(.data = fvar_df, .fun = ncvar_def, units = "", dim = list(x_dim, y_dim, z_dim, t_dim),
                          prec = "float", compression = 7, chunksizes = c(length(x1), length(y1), 1, 1), missval=-999.0)
-  
+
   # and also create integer variables the same way and save them in a single list
   out_ivar_list <- mlply(.data = ivar_df, .fun = ncvar_def, units = "", dim = list(x_dim, y_dim, z_dim, t_dim),
                          prec = "integer", compression = 7, chunksizes = c(length(x1), length(y1), 1, 1), missval=-99)
-  
+
   out_var_list <- append(out_fvar_list, out_ivar_list)
-  
+
   #------------------------------- WRITING TO THE FILE --------------------------#
   #open output netcdf file
+  fname_split <- unlist(strsplit(flist[1], "_")) #split the fileName
   outfName <- str_replace(flist[1], pattern = fname_split[length(fname_split)], ".nc")
   outfPath <- paste(outdir, basename(outfName), sep="")
   ofile <- nc_create(filename = outfPath, vars =append(out_var_list, radar_latlon_var))
-  
+
   #write radar lat-lon to the file
   ncvar_put(nc=ofile, varid = "radar_latitude", vals = radar_lat1)
   ncvar_put(nc=ofile, varid = "radar_longitude", vals = radar_lon1)
-  
+
   print(paste("writing data in", basename(outfPath)))
   pb = txtProgressBar(min =1, max = length(flist), initial = 1, style = 3) #progress bar
   for (f in 1:length(flist)){
     setTxtProgressBar(pb, f)
     infile <- nc_open(flist[f])
-    
+
     #get the data from input netcdf and put all float variables in output file
     for(var in fvar_names){
       data <- ncvar_get(infile, varid = var)
       ncvar_put(nc = ofile, varid = var, vals = data, start = c(1, 1, 1, f),
                 count = c(length(x1), length(y1), length(z1), 1))
     }
-    
+
     #get the data from input netcdf and put all integer variables in output file.
     for(var in ivar_names){
       data <- ncvar_get(infile, varid = var)
@@ -145,7 +145,7 @@ while(length(flist_all)>0) {
     nc_close(infile)
   }
   cat("\n")
-  
+
   #close file
   nc_close(ofile)
 } # while loop ends
